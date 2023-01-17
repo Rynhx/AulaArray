6d95d931afc11fac75ed9d29f91dcedb49e2843f
Dara <dara.lima2@outlook.com>
2023-01-16 21:45:33 -0300

    aula

---
+++ aula01/aula.array/script.js
@@ -0,0 +1,41 @@
+
+ 
+  var colors = ["pink", "black", "green", "purple", "yellow", "white", "blue", "red", (2 > 1), 1];
+  var primeiro = colors [0]
+  var ultimo = colors[colors.length - 1];
+
+  for (let colors = 0; colors < colors.length; red++) {
+    colors.push(colors[red]);
+  }
+  colors.forEach(element => console.log(element));
+  console.log(primeiro);
+  console.log(ultimo);
+  console.log(colors.length - 1);
+
+  
+  var meuArrayMutante = colors.map((element) => 2);
+  console.log(meuArrayMutante);
+
+  var cores = ["verde", "laranja", "vermelho", "azul"];
+  const upper = cores.map(color => {
+    return color.toUpperCase ();})
+    console.log (upper);
+
+    for (let cores = 0; cores < cores.length; verde, laranja, vermelho, azul++) {
+      cores.push(cores["verde", "laranja", "vermelho", "azul"]);};
+
+
+
+  
+  
+  
+
+ 
+
+  
+
+  
+
+ 
+
+
